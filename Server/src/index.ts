afe40f0ff9d43f4c99a98584104c9ba3b3a7788d
syafiq096 <syafiq513096@gmail.com>
2021-05-05 05:02:00 +0800

    (add) connextion to graphql UI

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import { schema } from './Schema';
 import cors from 'cors';
 import { createConnection } from 'typeorm';
 
@@ -14,14 +15,14 @@ const main = async () => {
         synchronize: false,
         entities: [],
     });
-    
+
     const app = express()
     app.use(cors())
     app.use(express.json())
-    // app.use("/graphql", graphqlHTTP({
-    //     schema,
-    //     graphiql: true
-    // }))
+    app.use("/graphql", graphqlHTTP({
+        schema,
+        graphiql: true
+    }))
 
     app.listen(3001, () => {
         console.log(`SERVER RUNNING.....`)
