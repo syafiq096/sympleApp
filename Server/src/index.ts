bef109404ecab2986c9068506d4fa58280a4daef
syafiq096 <syafiq513096@gmail.com>
2021-05-05 04:19:12 +0800

    setting up index.ts

---
+++ Server/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import cors from 'cors';
+import { createConnection } from 'typeorm';
+
+const main = async () => {
+    const app = express()
+    app.use(cors())
+    app.use(express.json())
+    // app.use("/graphql", graphqlHTTP({
+    //     schema,
+    //     graphiql: true
+    // }))
+
+    app.listen(3001, () => {
+        console.log(`SERVER RUNNING.....`)
+    })
+
+}
+
+main().catch((err) => {
+    console.log(err)
+});
