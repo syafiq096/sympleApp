e050a48768cecae1bce36878c4c3ce831c40deff
syafiq096 <syafiq513096@gmail.com>
2021-05-06 00:54:49 +0800

    (add) search user query

--- Server/src/Schema/Queries/User.ts
+++ Server/src/Schema/Queries/User.ts
@@ -1,11 +1,22 @@
-import { GraphQLList } from 'graphql'
+import { GraphQLList, GraphQLString } from 'graphql'
 import { UserType } from '../TypeDefs/User'
 import { Users } from '../../Entities/User'
+import { Like } from 'typeorm';
 
 
 export const GET_ALL_USERS = {
     type: new GraphQLList(UserType),
     resolve() {
         return Users.find();
+    }
+}
+
+export const SEARCH_USERS = {
+    type: new GraphQLList(UserType),
+    args: {
+        name: {type: GraphQLString},
+    },
+    resolve(parent: any, args: any) {
+        return Users.find({name: Like(`%${args.name}%`)});
     }
 }
--- Server/src/Schema/index.ts
+++ Server/src/Schema/index.ts
@@ -1,12 +1,13 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
-import {GET_ALL_USERS} from './Queries/User';
+import {GET_ALL_USERS, SEARCH_USERS} from './Queries/User';
 import {CREATE_USER, DELETE_USER, UPDATE_PASSWORD} from './Mutations/User';
 
 
 const RootQuery = new GraphQLObjectType({
     name: "RootQuery",
     fields: {
-        getAllUsers: GET_ALL_USERS
+        getAllUsers: GET_ALL_USERS,
+        searchUsers: SEARCH_USERS
     }
 })
 
