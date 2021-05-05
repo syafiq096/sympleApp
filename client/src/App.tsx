1bc04a56f2e012b9803797c0b99d4369ffffb7e3
syafiq096 <syafiq513096@gmail.com>
2021-05-06 00:55:46 +0800

    Update App.tsx
    (add) ui integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useMutation} from '@apollo/client';
+import {CREATE_USER} from './GraphQL/Mutation'
 import './App.css';
 
 function App() {
+
+  const [createUser, {error}] = useMutation(CREATE_USER);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    userName: "",
+    password: ""
+  });
+
+  const handleSubmit =()=> {
+    createUser(
+      {variables: {
+        name: userInfo.name, 
+        username: userInfo.userName, 
+        password: userInfo.password}
+      })
+  }
+
+
+  
   return (
-    <div className="App">
-   
-    </div>
+    
+      <div className="createUser">
+        <input type="text" placeholder="Name" onChange={e => setUserInfo({...userInfo, name: e.target.value})} />
+        <input type="text" placeholder="User Name" onChange={e => setUserInfo({...userInfo, userName: e.target.value})} />
+        <input type="text" placeholder="Password" onChange={e => setUserInfo({...userInfo, password: e.target.value})} />
+        <button onClick={handleSubmit}>Create User</button>
+      </div>
   );
 }
 
