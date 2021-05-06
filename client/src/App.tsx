2b95226c821d54b9ab6db99e204925419b207ddf
syafiq096 <syafiq513096@gmail.com>
2021-05-07 04:34:54 +0800

    (fix) remove redundant items

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,8 +12,6 @@ function App() {
   const [search, setSearch] = useState("")
   const [getUser, { data, loading, refetch }] = useLazyQuery(GET_ALL_USERS);
 
-  console.log('search :>> ', search);
-
   useEffect(() => {
     getUser({variables: {name: ""}})
   }, [getUser])
@@ -24,19 +22,11 @@ function App() {
       <Container maxWidth="xl">
         <Box mt={2} />
         <UserForm refetch={refetch} />
-        {/* <div className="createUser">
-        <input type="text" placeholder="Name" onChange={e => setUserInfo({...userInfo, name: e.target.value})} />
-        <input type="text" placeholder="User Name" onChange={e => setUserInfo({...userInfo, userName: e.target.value})} />
-        <input type="text" placeholder="Password" onChange={e => setUserInfo({...userInfo, password: e.target.value})} />
-        <button onClick={handleSubmit}>Create User</button>
-        </div> */}
         <Box mt={3} />
         <Divider variant="middle" />
         <Box mt={3} />
-
         <List data={data?.getAllUsers} refetch={refetch} loading={loading} setSearch={setSearch} search={search} />
         <Box mt={2} />
-        <UpdateUser />
       </Container>
     </div>
   );
