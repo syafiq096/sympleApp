0165238d3ea147e492e408ff78da9f3a260060bf
syafiq096 <syafiq513096@gmail.com>
2021-05-06 14:23:13 +0800

    (add) user table

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import {useMutation} from '@apollo/client';
 import {CREATE_USER} from './GraphQL/Mutation'
 import './App.css';
 
+import List from './View/TableList/Index';
+
 function App() {
 
   const [createUser, {error}] = useMutation(CREATE_USER);
@@ -30,6 +32,7 @@ function App() {
         <input type="text" placeholder="User Name" onChange={e => setUserInfo({...userInfo, userName: e.target.value})} />
         <input type="text" placeholder="Password" onChange={e => setUserInfo({...userInfo, password: e.target.value})} />
         <button onClick={handleSubmit}>Create User</button>
+        <List />
       </div>
   );
 }
