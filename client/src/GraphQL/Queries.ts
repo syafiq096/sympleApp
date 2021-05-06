e8d16a98d6826e8c8dc5107cc716eb194bea7319
syafiq096 <syafiq513096@gmail.com>
2021-05-07 03:07:12 +0800

    (add) get user api to accept param

--- client/src/GraphQL/Queries.ts
+++ client/src/GraphQL/Queries.ts
@@ -1,13 +1,11 @@
 import {gql} from "@apollo/client"
 
 export const GET_ALL_USERS = gql`
- 
-query getAllUsers {
-    getAllUsers {
-        id
-        name
-        username
+  query getAllUsers($name: String!) {
+    getAllUsers(name: $name) {
+      id
+      name
+      username
     }
-}
-
-`
+  }
+`;
