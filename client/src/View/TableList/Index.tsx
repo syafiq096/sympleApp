a40775994af32b5113b62326269955b4f39b56aa
syafiq096 <syafiq513096@gmail.com>
2021-05-06 23:57:47 +0800

    (add) table integration

--- client/src/View/TableList/Index.tsx
+++ client/src/View/TableList/Index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { DELETE_USER } from "../../GraphQL/Mutation";
 import { useMutation } from "@apollo/client";
 
+import TableList from '../../Components/Table'
+import { IconButton, SvgIcon, Tooltip } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu'
+
 interface compData {
     data?: any;
     refetch: () => {}
@@ -14,16 +18,35 @@ function Index({data = [{}], refetch}: compData) {
     deleteSelectedUser({variables: {id: id}}).then(() => refetch())
   }
 
+  const renderDelete = (id: number) => {
+    return (
+      <div>
+          <Tooltip title="Invoice" aria-label="Update">
+            <IconButton onClick={() => deleteUser(id)}>
+              <SvgIcon fontSize="small">
+                <MenuIcon />
+              </SvgIcon>
+            </IconButton>
+          </Tooltip>
+      </div>
+    );
+  };
+
   return (
     <div>
-      {data.map((item: any) => {
-        return (
-          <div key={item.id}>
-            {item.name} / {item.username}
-            <button onClick={() => deleteUser(item.id)}>Delete User</button>
-          </div>
-        );
-      })}
+      <TableList 
+      data={
+        data.map((item: any , index: number) => {
+          return [
+            index +1,
+            item.name,
+            item.username,
+            renderDelete(item.id)
+          ]
+      })} 
+      header={['No', 'Name', 'Username', 'Action']}
+      align={['center, center, center, center']}
+       />
     </div>
   );
 }
